bd964b22220285e9e56c4f833021d289c841314c
mark.tupas@geo.tuwien.ac.at <mark.tupas@geo.tuwien.ac.at>
2023-03-03 17:56:08 +0100

    add zipped geotiff read-- changed existence and bigtiff checks

--- src/veranda/raster/native/geotiff.py
+++ src/veranda/raster/native/geotiff.py
@@ -2,6 +2,7 @@
 
 import os
 import struct
+from zipfile import ZipFile
 import numpy as np
 from osgeo import gdal
 from typing import List
@@ -128,8 +129,15 @@ class GeoTiffFile:
             True if the given file is a BigTIFF, else False.
 
         """
-        with open(filepath, 'rb') as f:
-            header = f.read(4)
+        if '.zip' in filepath:
+            filepath = filepath.lstrip('/vsizip/')  # removes gdal virtual file system prefix to open w ZipFile
+            zip_filepath, inzip_filepath = filepath.split('.zip/')
+            with ZipFile(zip_filepath + '.zip') as zip:
+                with zip.open(inzip_filepath, 'r') as f:
+                    header = f.read(4)  # tests reads bytes not str, no need covert to bytearray
+        else:
+            with open(filepath, 'rb') as f:
+                header = f.read(4)
         byteorder = {b'II': '<', b'MM': '>', b'EP': '<'}[header[:2]]
         version = struct.unpack(byteorder + "H", header[2:4])[0]
         return version == 43
@@ -171,7 +179,19 @@ class GeoTiffFile:
 
         """
         if self.mode == 'r':
-            if not os.path.exists(self.filepath):
+            if '.zip' in self.filepath:
+                self.filepath = self.filepath.lstrip('/vsizip/')  # removes gdal virtual file system prefix if its there
+                zip_filepath, inzip_filepath = self.filepath.split('.zip/')
+                zip_filepath += '.zip'
+                if not os.path.exists(zip_filepath):  # checks zip file
+                    err_msg = f"File '{zip_filepath}' does not exist."
+                    raise FileNotFoundError(err_msg)
+                with ZipFile(zip_filepath) as zip:
+                    if inzip_filepath not in zip.namelist():  # checks file in zip
+                        err_msg = f"File '{self.filepath}' does not exist."
+                        raise FileNotFoundError(err_msg)
+                self.filepath = '/vsizip/' + self.filepath  # adds the gdal vsi prefix
+            elif not os.path.exists(self.filepath):
                 err_msg = f"File '{self.filepath}' does not exist."
                 raise FileNotFoundError(err_msg)
             self.src = gdal.Open(self.filepath, gdal.GA_ReadOnly)
